c9079efa7db32759482ec4de788930a8030681de
TesnSai <shivamchaudhary8476@gmail.com>
2024-04-11 23:50:30 +0530

    form implementataion

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SignupForm from "./SignupForm";
 
 const Signup = () => {
@@ -7,8 +8,7 @@ const Signup = () => {
             <div className="w-[20%] h-max flex flex-col items-center justify-center bg-white rounded-md">
                 <h1 className="text-4xl font-bold mt-4">Sign Up</h1>
                 <p className="text-center my-2 font-medium text-gray-500">Enter your information to create an account</p>
-                <SignupForm/>
-                <button className="w-[90%] rounded-md bg-black text-white py-2 mb-4">Sign Up</button>
+                <SignupForm />
                 <p className="mb-4">Already have an account? <span>Login</span></p>
             </div>
         </div>
--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const SignupForm = () => {
+
     const initalValues = {
         firstName: "",
         lastName: "",
@@ -10,33 +11,59 @@ const SignupForm = () => {
     
     const [formValues, setFormValues] = useState(initalValues);
     
+    async function handleSignup(){
+        console.log(formValues);
+        // const requestBody = {
+        //     username: formValues.email,
+        //     firstName: formValues.firstName,
+        //     lastName: formValues.lastName,
+        //     password: formValues.password
+        // }
+
+        // const response = await fetch("http://localhost:3000/api/v1/user/signup", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify(requestBody),
+        // })
+
+        // const data = await response.json();
+        // console.log(data);
+    }
+
+    console.log(formValues);
+    
     return (
-        <div className="my-4 flex flex-col w-[90%] gap-2 [&>input]:border rounded-md [&>label]:font-medium">
-            <label>
-                First Name
-            </label>
-            <input placeholder="John" type="text" className="pl-1 placeholder:pl-2 py-1"
-                value={formValues.firstName} onChange={(e) => setFormValues((...prev) => prev.firstName = e.target.value)}>
-            </input>
-            <label>
-                Last Name
-            </label>
-            <input placeholder="Doe" type="text" className="pl-1 placeholder:pl-2 py-1"
-                value={formValues.lastName} onChange={(e) => setFormValues((...prev) => prev.lastName = e.target.value)}>
-            </input>
-            <label>
-                Email
-            </label>
-            <input placeholder="johndoe@example.com" type="email" className="pl-1 placeholder:pl-2 py-1"
-                value={formValues.email} onChange={(e) => setFormValues((...prev) => prev.email = e.target.value)}>
-            </input>
-            <label>
-                Password
-            </label>
-            <input type="password" className="pl-1 placeholder:pl-2 py-1"
-                value={formValues.password} onChange={(e) => setFormValues((...prev) => prev.password = e.target.value)}>
-            </input>
-        </div>
+        <>
+            <div className="my-4 flex flex-col w-[90%] gap-2 [&>input]:border rounded-md [&>label]:font-medium">
+                <label>
+                    First Name
+                </label>
+                <input placeholder="John" type="text" className="pl-1 placeholder:pl-2 py-1"
+                    value={formValues.firstName} onChange={(e) => setFormValues((...prev) => prev.firstName = e.target.value)}>
+                </input>
+                <label>
+                    Last Name
+                </label>
+                <input placeholder="Doe" type="text" className="pl-1 placeholder:pl-2 py-1"
+                    value={formValues.lastName} onChange={(e) => setFormValues((...prev) => prev.lastName = e.target.value)}>
+                </input>
+                <label>
+                    Email
+                </label>
+                <input placeholder="johndoe@example.com" type="email" className="pl-1 placeholder:pl-2 py-1"
+                    value={formValues.email} onChange={(e) => setFormValues((...prev) => prev.email = e.target.value)}>
+                </input>
+                <label>
+                    Password
+                </label>
+                <input type="password" className="pl-1 placeholder:pl-2 py-1"
+                    value={formValues.password} onChange={(e) => setFormValues((prevState) => ({...prevState,}))}>
+                </input>
+            </div>
+            <button className="w-[90%] rounded-md bg-black text-white py-2 mb-4" onClick={handleSignup}>Sign Up</button>
+        </>
     )
 }
 
