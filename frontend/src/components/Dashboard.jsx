02371db55a6d045757f9322e5a5989c0aa5a16a9
SurajSingh <shivamchaudhary8476@gmail.com>
2024-05-19 17:39:22 +0530

    implemented send money modal

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react";
 import Users from "./Users";
+import SendMoney from "./SendMoney";
 
 const Dashboard = () => {
     const authToken = localStorage.getItem("token");
     const [userList, setUserList] = useState([]);
+    const [userBalance, setuserBalance] = useState("");
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
+        getUserBalance();
         getUserList();
     }, [])
 
+    async function getUserBalance(){
+        const response = await fetch(`http://localhost:3000/api/v1/account/balance`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${authToken}`
+            },
+        })
+
+        const data = await response.json();
+        setuserBalance(data?.balance);
+    }
+
     async function getUserList(){
-        console.log("auth token", authToken);
-        const filter = "Suraj";
-        const response = await fetch(`http://localhost:3000/api/v1/user/bulk?${filter}`, {
+        const response = await fetch(`http://localhost:3000/api/v1/user/bulk`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -21,27 +36,31 @@ const Dashboard = () => {
         })
         const data = await response.json();
         setUserList(data?.users);
-        console.log("res data: ", data);
     }
 
 
     return (
-        <div className="w-screen h-screen flex justify-center">
-            <div className="w-[90%] h-auto flex-col flex items-center">
-                <div className="w-full flex justify-between mt-10">
-                    <h1 className="font-bold text-2xl">Payments App</h1>
-                    <div className="flex items-center gap-2 font-medium text-xl">
-                        <p>Hello, User</p>
-                        <div className="w-8 h-8 rounded-full bg-slate-200 flex items-center justify-center">U</div>
+        <>
+            {!showModal && 
+            <div className="w-screen h-screen flex justify-center">
+                <div className="w-[90%] h-auto flex-col flex items-center">
+                    <div className="w-full flex justify-between mt-10">
+                        <h1 className="font-bold text-2xl">Money Magnet</h1>
+                        <div className="flex items-center gap-2 font-medium text-xl">
+                            <p>Hello, User</p>
+                            <div className="w-8 h-8 rounded-full bg-slate-200 flex items-center justify-center">U</div>
+                        </div>
                     </div>
+                    <hr className="h-[2px] w-full bg-slate-200 my-8"></hr>
+                    <div className="w-full flex items-start font-bold text-xl">
+                        <p>Your Balance: <span className="text-green-500">₹{parseInt(userBalance).toFixed(2)}</span></p>
+                    </div>
+                    <Users usersList={userList} setShowModal={setShowModal}/>
                 </div>
-                <hr className="h-[2px] w-full bg-slate-200 my-8"></hr>
-                <div className="w-full flex items-start font-bold text-xl">
-                    <p>Your Balance -----</p>
                 </div>
-                <Users usersList={userList}/>
-            </div>
-        </div>
+            }
+            <SendMoney open={showModal} setShowModal={setShowModal}/>
+        </>
     )
 }
 
--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -1,7 +1,27 @@
-const SendMoney = () => {
+const SendMoney = ({ open, setShowModal }) => {
+    const firstName = localStorage.getItem("firstName");
+    const lastName = localStorage.getItem("lastName");
+    const iconText = firstName.charAt(0).toUpperCase();
 
+    const visibility = open ? "visible": "hidden";
     return(
-        <></>
+        <div className={`w-screen h-screen flex items-center justify-center ${visibility}`}>
+            <div className="w-[30%] flex flex-col justify-between items-center rounded-md shadow-xl">
+                <h1 className="text-4xl font-bold mt-5">Send Money</h1>
+                <div className="mt-10 flex items-start w-[85%] gap-4">
+                    <div className="w-12 h-12 rounded-full flex items-center justify-center bg-green-500 text-xl font-semibold text-white">{iconText}</div>
+                    <p className="mt-1 text-2xl font-bold">{firstName} {lastName}</p>
+                </div>
+                <div className="w-[85%] flex items-start">
+                    <p className="font-semibold">Amount (in Rs) </p>
+                </div>
+                <div className="w-[85%] mt-4 flex flex-col items-center justify-center gap-6">
+                    <input className="w-full h-10 border-slate-300 border rounded-md placeholder:pl-3" placeholder="Enter amount"></input>
+                    <button className="w-full h-10 bg-green-500 text-white rounded-md">Initiate Transfer</button>
+                    <button className="w-full h-10 bg-red-500 text-white rounded-md mb-10" onClick={() => setShowModal(false)}>Cancel</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
